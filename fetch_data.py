15c9d71e186acb42fac5d2aa909ff5ae24c364f5
Olivier Grisel <olivier.grisel@ensta.org>
2013-03-13 13:38:08 -0700

    Fetch covertype dataset

--- fetch_data.py
+++ fetch_data.py
@@ -1,7 +1,12 @@
+import numpy as np
 import os
 import urllib
 import tarfile
+import zipfile
+import gzip
 from sklearn.datasets import load_files
+from sklearn.externals import joblib
+
 
 TWENTY_URL = ("http://people.csail.mit.edu/jrennie/"
               "20Newsgroups/20news-bydate.tar.gz")
@@ -9,6 +14,14 @@ TWENTY_ARCHIVE_NAME = "20news-bydate.tar.gz"
 TWENTY_CACHE_NAME = "20news-bydate.pkz"
 TWENTY_TRAIN_FOLDER = "20news-bydate-train"
 TWENTY_TEST_FOLDER = "20news-bydate-test"
+
+SENTIMENT140_URL = ("http://cs.stanford.edu/people/alecmgo/"
+                    "trainingandtestdata.zip")
+SENTIMENT140_ARCHIVE_NAME = "trainingandtestdata.zip"
+
+
+COVERTYPE_URL = ('http://archive.ics.uci.edu/ml/'
+                 'machine-learning-databases/covtype/covtype.data.gz')
 
 
 def get_datasets_folder():
@@ -42,11 +55,69 @@ def check_twenty_newsgroups(datasets_folder):
         print("Decompressing %s" % archive_path)
         tarfile.open(archive_path, "r:gz").extractall(path=datasets_folder)
 
-    print("Check loading the 20 newsgroups files...")
-    load_files(train_path, charset='latin1')
-    load_files(test_path, charset='latin1')
-    print("Success!")
+    print("Checking that the 20 newsgroups files exist...")
+    assert os.path.exists(train_path)
+    assert os.path.exists(test_path)
+    print("=> Success!")
+
+
+def check_sentiment140(datasets_folder):
+    print("Checking availability of the sentiment 140 dataset")
+    archive_path = os.path.join(datasets_folder, SENTIMENT140_ARCHIVE_NAME)
+    sentiment140_path = os.path.join(datasets_folder, 'sentiment140')
+    train_path = os.path.join(sentiment140_path,
+        'training.1600000.processed.noemoticon.csv')
+    test_path = os.path.join(sentiment140_path,
+        'testdata.manual.2009.06.14.csv')
+
+    if not os.path.exists(archive_path):
+        print("Downloading dataset from %s (77MB)" % SENTIMENT140_URL)
+        opener = urllib.urlopen(SENTIMENT140_URL)
+        open(archive_path, 'wb').write(opener.read())
+    else:
+        print("Found archive: " + archive_path)
+
+    if not os.path.exists(sentiment140_path):
+        print("Extracting %s to %s" % (archive_path, sentiment140_path))
+        zf = zipfile.ZipFile(archive_path)
+        zf.extractall(sentiment140_path)
+    print("Checking that the sentiment 140 CSV files exist...")
+    assert os.path.exists(train_path)
+    assert os.path.exists(test_path)
+    print("=> Success!")
+
+
+def check_covertype(datasets_folder):
+    print("Checking availability of the covertype dataset")
+    covtype_dir = os.path.join(datasets_folder, "covertype")
+    archive_path = os.path.join(covtype_dir, 'covtype.data.gz')
+    samples_path = os.path.join(covtype_dir, "samples.pkl")
+    targets_path = os.path.join(covtype_dir, "targets.pkl")
+
+    if not os.path.exists(covtype_dir):
+        os.makedirs(covtype_dir)
+
+    if not os.path.exists(archive_path):
+        print("Downloading dataset from %s (10.7MB)" % COVERTYPE_URL)
+        open(archive_path, 'wb').write(urllib.urlopen(COVERTYPE_URL).read())
+    else:
+        print("Found archive: " + archive_path)
+
+    if not os.path.exists(samples_path) or not os.path.exists(targets_path):
+        print("Parsing the data and splitting input and labels...")
+        f = open(archive_path, 'rb')
+        Xy = np.genfromtxt(gzip.GzipFile(fileobj=f), delimiter=',')
+
+        X = Xy[:, :-1]
+        y = Xy[:, -1].astype(np.int32)
+
+        joblib.dump(X, samples_path, compress=9)
+        joblib.dump(y, targets_path, compress=9)
+    print("=> Success!")
+
 
 if __name__ == "__main__":
     datasets_folder = get_datasets_folder()
     check_twenty_newsgroups(datasets_folder)
+    check_sentiment140(datasets_folder)
+    check_covertype(datasets_folder)
